5214fa086528ae31c709cbf05f2a4c984633cf08
Swiatlon <wiercikdoc@gmail.com>
2021-10-25 22:42:07 +0200

    Simple settings for styles

---
+++ src/assets/theme/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+    lightGrey: '#F7F8FA',
+    grey: '#C0C7D6',
+    darkGrey: '#737C8E',
+    black: '#111111',
+    goodAnswer: '#8FCB81',
+    badAnswer: '#CB8581',
+  },
+  fontSize: {
+    xl: '24px',
+    l: '19px',
+    m: '14px',
+    s: '11px',
+  },
+};
