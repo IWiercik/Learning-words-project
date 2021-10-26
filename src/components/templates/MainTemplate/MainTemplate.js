984293f6dc66485cc35e59983a1f2a3a96bb7a00
Swiatlon <wiercikdoc@gmail.com>
2021-10-26 20:43:34 +0200

    Added handler of switching content with react-router-dom

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Navigation from 'components/organisms/Navigation/Navigation';
 import { Wrapper, CenteredContent } from './MainTemplate.style';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import RegistrationForm from 'views/RegistrationForm/Registration';
+import LoginForm from 'views/LoginForm/LoginForm';
 
-const MainTemplate = ({ children }) => (
-  <Wrapper>
-    <Navigation></Navigation>
-    <CenteredContent>{children}</CenteredContent>
-  </Wrapper>
+const MainTemplate = () => (
+  <Router>
+    <Wrapper>
+      <Navigation></Navigation>
+      <CenteredContent>
+        <Switch>
+          <Route path="/login">
+            <LoginForm></LoginForm>
+          </Route>
+          <Route path="/">
+            <RegistrationForm></RegistrationForm>
+          </Route>
+        </Switch>
+      </CenteredContent>
+    </Wrapper>
+  </Router>
 );
 export default MainTemplate;
