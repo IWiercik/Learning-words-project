ac94baa6d4d67adf755130324cece599736b4384
Swiatlon <wiercikdoc@gmail.com>
2021-10-26 20:39:57 +0200

    Created styled component Input

---
+++ src/components/atoms/Input/Input.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  padding: 10px;
+  color: white;
+  background: rgb(255, 255, 255, 0.3);
+  border: none;
+  border-radius: 20px;
+  outline: none;
+`;
