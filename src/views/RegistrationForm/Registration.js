4058acc0b653c1765111c0a20fb8d5db46cdcbad
Swiatlon <wiercikdoc@gmail.com>
2021-10-26 20:44:20 +0200

    Created Registration component

---
+++ src/views/RegistrationForm/Registration.js
@@ -0,0 +1,5 @@
+import Form from 'components/organisms/Form.js/Form';
+const RegistrationForm = () => {
+  return <Form title="Create Account" submit="Register"></Form>;
+};
+export default RegistrationForm;
