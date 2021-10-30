930b48a3b684a867744b025c6cecdd3bb31ba622
Swiatlon <wiercikdoc@gmail.com>
2021-10-30 14:56:56 +0200

    Deleted commented code

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'assets/theme/theme';
 import { globalStyle as GlobalStyle } from 'assets/styles/globalStyle';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MainTemplate from 'components/templates/MainTemplate/MainTemplate';
 
 const Root = () => {
