b112e7e363532a64dad7c76a7e09ff311d8c7506
Swiatlon <wiercikdoc@gmail.com>
2021-10-30 14:52:22 +0200

    Added red border when validate is false and placeholder color

--- src/components/atoms/Input/Input.js
+++ src/components/atoms/Input/Input.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
-
 export const Input = styled.input`
   padding: 10px;
   color: white;
   background: rgb(255, 255, 255, 0.3);
-  border: none;
+  border: ${({ isValid }) => (isValid ? 'none' : '1px solid red')};
   border-radius: 20px;
   outline: none;
+  &::placeholder {
+    color: black;
+  }
 `;
