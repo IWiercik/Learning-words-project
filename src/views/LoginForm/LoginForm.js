7ca7e3fde23e31044db69a7ec538430da0117929
Swiatlon <wiercikdoc@gmail.com>
2021-10-30 14:55:45 +0200

    Created state,authorization and structure form

--- src/views/LoginForm/LoginForm.js
+++ src/views/LoginForm/LoginForm.js
@@ -1,5 +1,22 @@
 import Form from 'components/organisms/Form.js/Form';
+import { useState } from 'react';
 const LoginForm = () => {
-  return <Form title="Login" submit="Login"></Form>;
+  const initialState = { login: '', password: '' };
+  const [loginFormValues, setLoginFormValues] = useState(initialState);
+  const [authState, setAuthState] = useState({ login: 'Default', password: 'Default' });
+  const loginFormValidator = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <Form
+      name="loginForm"
+      title="Login"
+      submit="Login"
+      value={loginFormValues}
+      updateValuesMethod={setLoginFormValues}
+      formSubmitted={loginFormValidator}
+      dataAuth={authState}
+    ></Form>
+  );
 };
 export default LoginForm;
