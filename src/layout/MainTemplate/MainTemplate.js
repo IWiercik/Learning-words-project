c28260dc4a60106139aa2edcfe7ad178e6741064
Swiatlon <wiercikdoc@gmail.com>
2021-11-02 15:58:46 +0100

    Created initial view of MainTemplate component

--- src/layout/MainTemplate/MainTemplate.js
+++ src/layout/MainTemplate/MainTemplate.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import { signOut } from '@firebase/auth';
-import { auth } from 'firebase/firebase';
+import styled from 'styled-components';
+import Navigation from 'components/organisms/Navigation/Navigation';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import TypingMode from 'views/TypingMode/TypingMode';
+import LearningMode from 'views/LearningMode/LearningMode';
+const CenteredContent = styled.div`
+  align-self: center;
+  justify-self: center;
+  background: rgba(0, 0, 0, 0.397);
+  border-radius: 10px;
+  min-width: 300px;
+  position: relative;
+  display: grid;
+  box-shadow: 2px 2px 6px -1px rgb(0 0 0);
+  padding: 25px 15px;
+`;
+
 const MainTemplate = () => {
   return (
-    <React.Fragment>
-      <h1>User:</h1>
-      <button
-        onClick={function () {
-          signOut(auth);
-        }}
-      >
-        Wyloguj
-      </button>
-    </React.Fragment>
+    <Router>
+      <Navigation isAuthorized={true}></Navigation>
+      <CenteredContent>
+        <Switch>
+          <Route exact path="/typing" component={TypingMode}></Route>
+          <Route exact path="/learning" component={LearningMode}></Route>
+          <Route component={TypingMode} />
+        </Switch>
+      </CenteredContent>
+    </Router>
   );
 };
 export default MainTemplate;
