db24b4539a676a53ffceeec79ab9dd25e09a4918
Swiatlon <wiercikdoc@gmail.com>
2021-11-02 15:52:52 +0100

    Old file Input with added autofill styles

---
+++ src/components/atoms/AuthInput/AuthInput.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+export const Input = styled.input`
+  padding: 10px;
+  background: none !important;
+  border: none;
+  outline: none;
+  border-bottom: ${({ isValid }) => (isValid ? '1px solid white' : '1px solid red')};
+  font-size: ${({ theme }) => theme.fontSize.m};
+  &::placeholder {
+    color: white;
+  }
+  &:-webkit-autofill,
+  &:-webkit-autofill:hover,
+  &:-webkit-autofill:focus,
+  &:-webkit-autofill:active {
+    transition: background-color 50000s;
+    -webkit-text-fill-color: white !important;
+  }
+`;
