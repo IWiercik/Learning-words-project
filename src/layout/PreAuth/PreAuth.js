ecd93047327006379c77dc2d0f28e6383f023931
Swiatlon <wiercikdoc@gmail.com>
2021-11-02 16:01:08 +0100

    Added isAuthorized

--- src/layout/PreAuth/PreAuth.js
+++ src/layout/PreAuth/PreAuth.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import Navigation from 'components/organisms/Navigation/Navigation';
-import PageNotFound from 'components/organisms/PageNotFound/PageNotFound';
-import { Wrapper, CenteredContent } from './PreAuth.style';
+import { CenteredContent } from './PreAuth.style';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import RegistrationForm from 'views/RegistrationForm/Registration';
 import LoginForm from 'views/LoginForm/LoginForm';
 
 const preAuth = () => (
   <Router>
-    <Wrapper>
-      <Navigation></Navigation>
-      <CenteredContent>
-        <Switch>
-          <Route exact path="/login" component={LoginForm} />
-          <Route exact path="/register" component={RegistrationForm} />
-          <Route component={PageNotFound} />
-        </Switch>
-      </CenteredContent>
-    </Wrapper>
+    <Navigation isAuthorized={false}></Navigation>
+    <CenteredContent>
+      <Switch>
+        <Route exact path="/login" component={LoginForm} />
+        <Route exact path="/register" component={RegistrationForm} />
+        <Route component={LoginForm} />
+      </Switch>
+    </CenteredContent>
   </Router>
 );
 export default preAuth;
