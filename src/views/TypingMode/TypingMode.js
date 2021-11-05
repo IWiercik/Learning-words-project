f1f4cb70d7088fec1b818d115b7d0b60710f842e
Swiatlon <wiercikdoc@gmail.com>
2021-11-05 20:43:13 +0100

    Added clickHandler with addingData func, created state for controlling inputs value

--- src/views/TypingMode/TypingMode.js
+++ src/views/TypingMode/TypingMode.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Title } from 'components/atoms/Title/Title';
 import FieldWithInput from 'components/molecules/FieldWithInput/FieldWithInput';
 import styled from 'styled-components';
 import { Button } from 'components/atoms/Button/Button';
+import { AddData } from 'firebase/firebase';
+import { userContext } from 'providers/CurrentUser';
+import { useState } from 'react';
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -11,14 +14,39 @@ const Wrapper = styled.div`
 const ButtonContainer = styled.div`
   align-self: center;
 `;
-const TypingMode = () => (
-  <Wrapper>
-    <Title>Learning APP</Title>
-    <FieldWithInput textContent="English Word:"></FieldWithInput>
-    <FieldWithInput textContent="Polish Word:"></FieldWithInput>
-    <ButtonContainer>
-      <Button>ADD</Button>
-    </ButtonContainer>
-  </Wrapper>
-);
+const TypingMode = () => {
+  const initialState = { engWord: '', plWord: '' };
+  const [words, setWords] = useState(initialState);
+  const ctx = useContext(userContext);
+  // const userUID = ctx.currentUser.uid;
+  const userEmail = ctx.currentUser.email;
+  return (
+    <Wrapper>
+      <Title>Learning APP</Title>
+      <FieldWithInput
+        updateValuesMethod={setWords}
+        name="engWord"
+        words={words}
+        textContent="English Word:"
+      ></FieldWithInput>
+      <FieldWithInput
+        updateValuesMethod={setWords}
+        name="plWord"
+        words={words}
+        textContent="Polish Word:"
+      ></FieldWithInput>
+      <ButtonContainer>
+        <Button
+          onClick={() => {
+            AddData(userEmail, words);
+            console.log('Wysłano!');
+            setWords(initialState);
+          }}
+        >
+          ADD
+        </Button>
+      </ButtonContainer>
+    </Wrapper>
+  );
+};
 export default TypingMode;
