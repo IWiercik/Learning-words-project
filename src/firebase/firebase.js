634d04b90a1dcc83c89b23d1b1523f8447e035e4
Swiatlon <wiercikdoc@gmail.com>
2021-11-05 20:47:38 +0100

    Added firestore(firebase database) to project and created function that push user learning data to server

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import firebaseConfig from './firebaseConfig';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth } from '@firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { doc, getDoc, setDoc, updateDoc, arrayUnion } from '@firebase/firestore';
 
 const app = initializeApp(firebaseConfig);
 //AUTH, REGISTRATION + SIGN IN
@@ -29,3 +31,25 @@ export function signInUser(email, password) {
       console.warn(errorCode, errorMessage);
     });
 }
+//FireStore
+const db = getFirestore(app);
+const collection = 'Words';
+export const AddData = (userUID, userData) => {
+  const document = userUID;
+  const userRef = doc(db, collection, document);
+
+  const getData = getDoc(userRef).then((result) => {
+    //If document exist we update it
+    if (result.exists()) {
+      //Data Upated
+      updateDoc(userRef, { engWords: arrayUnion(userData.engWord) });
+      updateDoc(userRef, { plWords: arrayUnion(userData.plWord) });
+    } else {
+      //If Document not exist we need to create initial form
+      //Data created
+      const engWords = [userData.engWord];
+      const plWords = [userData.plWord];
+      setDoc(userRef, { engWords, plWords });
+    }
+  });
+};
