a1df47f70b5212896276c10b1991ad0926681bb0
Swiatlon <wiercikdoc@gmail.com>
2021-11-12 21:25:22 +0100

    Added redux Provider with store

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'index.css';
 import Root from 'views/Root/Root';
-import UserProvider from './providers/CurrentUser';
+import Providers from './providers/Providers';
+import { store } from 'store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserProvider>
-      <Root />
-    </UserProvider>
+    <Providers>
+      <Provider store={store}>
+        <Root />
+      </Provider>
+    </Providers>
   </React.StrictMode>,
   document.getElementById('root')
 );
