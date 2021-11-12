7235ce3b1ecbdb60987276f94d783d1d2054a7be
Swiatlon <wiercikdoc@gmail.com>
2021-11-12 21:34:16 +0100

    Added downloading user words data when app render

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -3,12 +3,22 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from 'assets/theme/theme';
 import { globalStyle as GlobalStyle } from 'assets/styles/globalStyle';
 import { Wrapper } from './Root.style';
-import { userContext } from 'providers/CurrentUser';
+import { appContext } from 'providers/Providers';
 import PreAuth from 'layout/PreAuth/PreAuth';
 import MainTemplate from 'layout/MainTemplate/MainTemplate';
+import { getData } from 'firebase/firebase';
+import { useDispatch } from 'react-redux';
+import { downloadData } from 'store';
+// import { updateData } from 'store';
+// import { listenForData } from 'firebase/firebase';
 
 const Root = () => {
-  const ctx = useContext(userContext);
+  // On the start app we download the user data and hear on snapshot
+  const ctx = useContext(appContext);
+  const dispatch = useDispatch();
+  ctx.currentUser && getData(ctx.currentUser.email).then((result) => (result ? dispatch(downloadData(result)) : false));
+  // ctx.currentUser && listenForData(ctx.currentUser.email);
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle> </GlobalStyle>
