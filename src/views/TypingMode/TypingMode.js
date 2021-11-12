e51d1a0847cc86fb9e63c9fa386a8831f7ea7d53
Swiatlon <wiercikdoc@gmail.com>
2021-11-12 21:34:41 +0100

    Used context in typing mode

--- src/views/TypingMode/TypingMode.js
+++ src/views/TypingMode/TypingMode.js
@@ -4,12 +4,12 @@ import FieldWithInput from 'components/molecules/FieldWithInput/FieldWithInput';
 import styled from 'styled-components';
 import { Button } from 'components/atoms/Button/Button';
 import { AddData } from 'firebase/firebase';
-import { userContext } from 'providers/CurrentUser';
+import { appContext } from 'providers/Providers';
 import { useState } from 'react';
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  row-gap: 20px;
+  row-gap: 25px;
 `;
 const ButtonContainer = styled.div`
   align-self: center;
@@ -17,12 +17,12 @@ const ButtonContainer = styled.div`
 const TypingMode = () => {
   const initialState = { engWord: '', plWord: '' };
   const [words, setWords] = useState(initialState);
-  const ctx = useContext(userContext);
+  const ctx = useContext(appContext);
   // const userUID = ctx.currentUser.uid;
   const userEmail = ctx.currentUser.email;
   return (
     <Wrapper>
-      <Title>Learning APP</Title>
+      <Title>Typing Mode</Title>
       <FieldWithInput
         updateValuesMethod={setWords}
         name="engWord"
