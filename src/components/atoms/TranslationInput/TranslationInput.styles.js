2153b4dac90836a1d071d0d2cd8cb42c85f8e0bf
Swiatlon <wiercikdoc@gmail.com>
2022-05-30 22:56:10 +0200

    Added style for use translation input

---
+++ src/components/atoms/TranslationInput/TranslationInput.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+export const InputBox = styled.label`
+  input {
+    border-radius: 10px;
+    padding: 9px 20px;
+    border: 1px solid white;
+    box-shadow: 8px 8px 24px 0px rgba(0, 0, 0, 1);
+    background: rgba(0, 0, 0, 0.397);
+    transition: 0.5s all;
+  }
+`;
