ca7355c9ce48af428c9d58569edb437d8c34b70a
Swiatlon <wiercikdoc@gmail.com>
2022-05-30 22:58:19 +0200

    Implemented translation input and transfered styles

--- src/views/LearningMode/LearningMode.js
+++ src/views/LearningMode/LearningMode.js
@@ -1,59 +1,26 @@
 import React from 'react';
 import { Title } from 'components/atoms/Title/Title';
 import { Text } from 'components/atoms/Text/Text';
-import styled from 'styled-components';
+import TranslationInput from 'components/atoms/TranslationInput/TranslationInput';
 import { Button } from 'components/atoms/Button/Button';
 import { useSelector } from 'react-redux';
 import { alertForHints } from 'helpers/sweetAlert';
+import { ButtonsBox, Wrapper } from './LearningMode.style';
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 15px;
-  text-align: center;
-  h4 {
-    color: ${({ answer }) => {
-      if (answer === 'Waiting') {
-        return 'Gray';
-      } else if (answer === 'Good') {
-        return 'Green';
-      } else if (answer === 'Wrong') {
-        return 'Red';
-      }
-    }};
-  }
-  input {
-    border-radius: 10px;
-    padding: 9px 20px;
-    border: 1px solid white;
-    box-shadow: 8px 8px 24px 0px rgba(0, 0, 0, 1);
-    background: rgba(0, 0, 0, 0.397);
-    transition: 0.5s all;
-  }
-  strong {
-    color: #008ff6;
-  }
-`;
-const ButtonsBox = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-`;
 const LearningMode = () => {
+  //Data with words
   const engWords = useSelector((state) => state.engWords);
   const plWords = useSelector((state) => state.plWords);
-  const answer = 'Waiting';
-  const actualWordToTranslate =
-    engWords && plWords ? engWords[Math.floor(Math.random() * engWords.length)] : 'Add your word!';
+  const actualWordToTranslate = engWords && plWords ? engWords[Math.floor(Math.random() * engWords.length)] : 'Add your word!';
   return (
-    <Wrapper answer={answer}>
+    <Wrapper answer="Waiting">
       <Title>Learning Mode</Title>
       <Text>
         English Word:
         <strong> {actualWordToTranslate}</strong>
       </Text>
-      <div>
-        <input placeholder="Your translation"></input>
-      </div>
+      {/* <TranslationInput value={userTranslationValue} onChange={handleUserTranslation}></TranslationInput> */}
+      <TranslationInput></TranslationInput>
       <h4>Waiting for translation...</h4>
       <ButtonsBox>
         <Button
