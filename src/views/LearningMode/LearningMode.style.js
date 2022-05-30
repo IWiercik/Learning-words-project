4080c2b813574b9c3f3d301e0c3064d77ace9b45
Swiatlon <wiercikdoc@gmail.com>
2022-05-30 22:58:56 +0200

    Created style file for learning mode component

---
+++ src/views/LearningMode/LearningMode.style.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+  text-align: center;
+  h4 {
+    color: ${({ answer }) => {
+      if (answer === 'Waiting') {
+        return 'Gray';
+      } else if (answer === 'Good') {
+        return 'Green';
+      } else if (answer === 'Wrong') {
+        return 'Red';
+      }
+    }};
+  }
+  strong {
+    color: #008ff6;
+  }
+`;
+export const ButtonsBox = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
