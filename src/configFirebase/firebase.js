26a7a0992d13319b8635c3d995511802b7e1e894
Swiatlon <wiercikdoc@gmail.com>
2022-06-02 17:04:49 +0200

    Added appearance of an alert

--- src/configFirebase/firebase.js
+++ src/configFirebase/firebase.js
@@ -3,6 +3,7 @@ import firebaseConfig from './firebaseConfig';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth } from '@firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { doc, getDoc, setDoc, updateDoc, arrayUnion } from '@firebase/firestore';
+import { alertForAddingWordsToDataBase } from 'helpers/sweetAlert';
 
 const app = initializeApp(firebaseConfig);
 //AUTH, REGISTRATION + SIGN IN
@@ -37,17 +38,19 @@ const collection = 'Words';
 export const AddData = (userUID, userData) => {
   const document = userUID;
   const userRef = doc(db, collection, document);
-  getDoc(userRef).then((result) => {
-    //If document exist we update it
-    if (result.exists()) {
-      updateDoc(userRef, { engWords: arrayUnion(userData.engWord), plWords: arrayUnion(userData.plWord) });
-    } else {
-      //If Document not exist we need to create initial form
-      const engWords = [userData.engWord];
-      const plWords = [userData.plWord];
-      setDoc(userRef, { engWords, plWords });
-    }
-  });
+  getDoc(userRef)
+    .then((result) => {
+      //If document exist we update it
+      if (result.exists()) {
+        updateDoc(userRef, { engWords: arrayUnion(userData.engWord), plWords: arrayUnion(userData.plWord) });
+      } else {
+        //If Document not exist we need to create initial form
+        const engWords = [userData.engWord];
+        const plWords = [userData.plWord];
+        setDoc(userRef, { engWords, plWords });
+      }
+    })
+    .then(alertForAddingWordsToDataBase);
 };
 
 export async function getData(userUID) {
