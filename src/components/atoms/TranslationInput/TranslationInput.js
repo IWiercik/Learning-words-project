154dd2acdf88ba8d2ef1aa7db657b1a1dff39944
Swiatlon <wiercikdoc@gmail.com>
2022-06-20 15:07:28 +0200

    Added functioning of passing input state to parent

--- src/components/atoms/TranslationInput/TranslationInput.js
+++ src/components/atoms/TranslationInput/TranslationInput.js
@@ -1,14 +1,17 @@
 import { InputBox } from './TranslationInput.styles';
 import { useState } from 'react';
-const TranslationInput = ({ value, onChange }) => {
+const TranslationInput = ({ value, onChange, sendData }) => {
   const initialTranslationValue = '';
   const [userTranslationValue, setUserTranslationValue] = useState(initialTranslationValue);
   const handleUserTranslation = (e) => {
     setUserTranslationValue(e.target.value);
   };
+  (function passDataToParent() {
+    sendData(userTranslationValue);
+  })();
   return (
     <InputBox>
-      <input type={'text'} value={userTranslationValue} onChange={handleUserTranslation}></input>
+      <input type={'text'} placeholder="Your translation" value={userTranslationValue} onChange={handleUserTranslation}></input>
     </InputBox>
   );
 };
