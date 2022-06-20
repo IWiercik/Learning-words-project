90c3f323c32ba0952b1988c2a88db3165ee15c2d
Swiatlon <wiercikdoc@gmail.com>
2022-06-20 15:11:04 +0200

    Added creating of word to translate when the user is authorized

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -9,13 +9,20 @@ import MainTemplate from 'layout/MainTemplate/MainTemplate';
 import { getData } from 'configFirebase/firebase';
 import { useDispatch } from 'react-redux';
 import { downloadData } from 'store/wordsSlice';
+import { updateWordToTranslate } from 'store/wordToTranslateSlice';
 
 const Root = () => {
   // On the start app we download the user data and hear on snapshot
   const ctx = useContext(appContext);
   const dispatch = useDispatch();
-  ctx.currentUser && getData(ctx.currentUser.email).then((result) => (result ? dispatch(downloadData(result)) : false));
-
+  ctx.currentUser &&
+    getData(ctx.currentUser.email).then((result) => {
+      if (result) {
+        const actualWordToTranslate = result.engWords[Math.floor(Math.random() * result.engWords.length)];
+        dispatch(downloadData(result));
+        dispatch(updateWordToTranslate(actualWordToTranslate));
+      }
+    });
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle> </GlobalStyle>
