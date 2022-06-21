c91982b3161e06fcbfc521e0716d252d760a1907
Swiatlon <wiercikdoc@gmail.com>
2022-06-21 23:00:30 +0200

    Added the clearing input mechanism

--- src/components/atoms/TranslationInput/TranslationInput.js
+++ src/components/atoms/TranslationInput/TranslationInput.js
@@ -1,14 +1,23 @@
 import { InputBox } from './TranslationInput.styles';
-import { useState } from 'react';
-const TranslationInput = ({ value, onChange, sendData }) => {
+import { useEffect, useState } from 'react';
+const TranslationInput = ({ sendData, reset }) => {
   const initialTranslationValue = '';
   const [userTranslationValue, setUserTranslationValue] = useState(initialTranslationValue);
   const handleUserTranslation = (e) => {
     setUserTranslationValue(e.target.value);
   };
+  useEffect(() => {
+    function clearingInput() {
+      if (reset === true) {
+        setUserTranslationValue('');
+      }
+    }
+    clearingInput();
+  });
   (function passDataToParent() {
     sendData(userTranslationValue);
   })();
+
   return (
     <InputBox>
       <input type={'text'} placeholder="Your translation" value={userTranslationValue} onChange={handleUserTranslation}></input>
