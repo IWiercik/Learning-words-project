7063e5772ca56c8ea0cc0d3a2ea2065009849866
Swiatlon <wiercikdoc@gmail.com>
2022-06-21 23:01:16 +0200

    Added animation

--- src/views/LearningMode/LearningMode.style.js
+++ src/views/LearningMode/LearningMode.style.js
@@ -1,4 +1,28 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+//Animations
+const dotEffect = keyframes`
+  0%{
+    opacity:0.7;
+    color: #00bbe6;
+  }
+  25%{
+    opacity:0.3 ;
+    color: #00a7cc;
+  }
+  50%{
+    opacity: 1 ;
+    color: #0092b3;
+  }
+  75%{
+    opacity: 0.3 ;
+    color: #008fae;
+  }
+  100%{
+    opacity: 0.7 ;
+    color: #007d99;
+  }
+`;
+//Styled
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -7,7 +31,7 @@ export const Wrapper = styled.div`
   h4 {
     color: ${({ answer }) => {
       if (answer === 'Waiting') {
-        return 'Gray';
+        return 'rgb(0, 143, 202)';
       } else if (answer === 'Correct') {
         return 'Green';
       } else if (answer === 'Wrong') {
@@ -22,4 +46,26 @@ export const Wrapper = styled.div`
 export const ButtonsBox = styled.div`
   display: flex;
   justify-content: space-evenly;
+  padding-bottom: 10px;
+`;
+
+export const LettersBox = styled.h4`
+  padding: 5px;
+  display: flex;
+  justify-content: center;
+  span {
+    min-width: 5px;
+  }
+  span:nth-child(29) {
+    animation: ${dotEffect} 2s backwards infinite;
+    animation-delay: 0.3s;
+  }
+  span:nth-child(30) {
+    animation: ${dotEffect} 2s backwards infinite;
+    animation-delay: 0.4s;
+  }
+  span:nth-child(31) {
+    animation: ${dotEffect} 2s backwards infinite;
+    animation-delay: 0.5s;
+  }
 `;
