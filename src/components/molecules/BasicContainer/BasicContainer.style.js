e14f4bea3e2de6dedfd4793412b8135e915241bb
Swiatlon <wiercikdoc@gmail.com>
2022-06-23 21:40:38 +0200

    Created containers for components

---
+++ src/components/molecules/BasicContainer/BasicContainer.style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+export const BasicContainer = styled.div`
+  align-self: center;
+  justify-self: center;
+  background: rgba(0, 0, 0, 0.397);
+  border-radius: 10px;
+  min-width: 300px;
+  position: relative;
+  display: grid;
+  box-shadow: 2px 2px 6px -1px rgb(0 0 0);
+  padding: 25px 25px;
+`;
---
+++ src/components/molecules/CenterContainer/CenterContainer.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+export const CenterContainer = styled.div`
+  align-self: center;
+  justify-self: center;
+  background: rgba(0, 0, 0, 0.397);
+  min-width: 300px;
+  position: relative;
+  display: grid;
+  box-shadow: 2px 2px 6px -1px rgb(0 0 0);
+`;
