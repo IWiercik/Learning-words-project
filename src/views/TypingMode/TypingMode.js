b8575c4af4b91cf4509ef56abdb17916a0d6db42
Swiatlon <wiercikdoc@gmail.com>
2022-06-23 21:50:24 +0200

    Added container and passing length of words data to AddData

--- src/views/TypingMode/TypingMode.js
+++ src/views/TypingMode/TypingMode.js
@@ -7,6 +7,9 @@ import { AddData } from 'configFirebase/firebase';
 import { appContext } from 'providers/Providers';
 import { useState } from 'react';
 import { alertForEmptyInput } from 'helpers/sweetAlert';
+import { BasicContainer } from 'components/molecules/BasicContainer/BasicContainer.style';
+import { useSelector } from 'react-redux';
+
 const Wrapper = styled.form`
   display: flex;
   flex-direction: column;
@@ -16,33 +19,35 @@ const ButtonContainer = styled.div`
   align-self: center;
 `;
 const TypingMode = () => {
+  const wordsDataLength = useSelector((state) => state.wordsDataSlice.engWords).length; // used to get the id of new item
   const initialState = { engWord: '', plWord: '' };
   const [words, setWords] = useState(initialState);
   const ctx = useContext(appContext);
 
   return (
-    <Wrapper>
-      <Title>Typing Mode</Title>
-      <FieldWithInput updateValuesMethod={setWords} name="engWord" words={words} textContent="English Word:"></FieldWithInput>
-      <FieldWithInput updateValuesMethod={setWords} name="plWord" words={words} textContent="Polish Word:"></FieldWithInput>
-      <ButtonContainer>
-        <Button
-          onClick={(e) => {
-            e.preventDefault();
-            if (words.engWord.length === 0 || words.plWord.length === 0) {
-              alertForEmptyInput();
-            } else {
-              const userEmail = ctx.currentUser.email;
-              AddData(userEmail, words);
-              console.log('Wysłano!');
-              setWords(initialState);
-            }
-          }}
-        >
-          ADD
-        </Button>
-      </ButtonContainer>
-    </Wrapper>
+    <BasicContainer>
+      <Wrapper>
+        <Title>Typing Mode</Title>
+        <FieldWithInput updateValuesMethod={setWords} name="engWord" words={words} textContent="English Word:"></FieldWithInput>
+        <FieldWithInput updateValuesMethod={setWords} name="plWord" words={words} textContent="Polish Word:"></FieldWithInput>
+        <ButtonContainer>
+          <Button
+            onClick={(e) => {
+              e.preventDefault();
+              if (words.engWord.length === 0 || words.plWord.length === 0) {
+                alertForEmptyInput();
+              } else {
+                const userEmail = ctx.currentUser.email;
+                AddData(userEmail, words, wordsDataLength);
+                setWords(initialState);
+              }
+            }}
+          >
+            ADD
+          </Button>
+        </ButtonContainer>
+      </Wrapper>
+    </BasicContainer>
   );
 };
 export default TypingMode;
