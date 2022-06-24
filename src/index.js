ad2ca91183b2134e946209964a0d5383c7ae1ae1
Swiatlon <wiercikdoc@gmail.com>
2022-06-25 00:02:35 +0200

    Commented React strict mode to avoid doubles calls of api

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Providers>
-      <Provider store={store}>
-        <Root />
-      </Provider>
-    </Providers>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Providers>
+    <Provider store={store}>
+      <Root />
+    </Provider>
+  </Providers>
+  // </React.StrictMode>
 );
