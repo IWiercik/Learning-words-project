aa337d017c6363f1add437f4296d0be7b6d78292
Swiatlon <wiercikdoc@gmail.com>
2022-06-28 14:57:02 +0200

    Reconstructed component and his styles

--- src/views/WordsControlPanel/WordsControlPanel.js
+++ src/views/WordsControlPanel/WordsControlPanel.js
@@ -1,12 +1,15 @@
 import { CenterContainer } from 'components/molecules/CenterContainer/CenterContainer.style';
 import deleteIcon from 'assets/images/icons/x-circle.svg';
 import editIcon from 'assets/images/icons/edit.svg';
+import arrowDown from 'assets/images/icons/arrow-down.svg';
+import arrowUp from 'assets/images/icons/arrow-up.svg';
 import { useSelector } from 'react-redux';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { appContext } from 'providers/Providers';
-import { Table } from './WordsControlPanel.style';
-import { Row } from './WordsControlPanel.style';
 import { deleteSingleData } from 'configFirebase/firebase';
+import { alertForEditingWords } from 'helpers/sweetAlert';
+import Input from 'components/atoms/Input/Input';
+import { Table, TableAdditionalOptions, TableItemsContainer, Row } from './WordsControlPanel.style';
 
 function WordsControlPanel() {
   const ctx = useContext(appContext);
@@ -14,47 +17,80 @@ function WordsControlPanel() {
   const engWords = useSelector((state) => state.wordsDataSlice.engWords);
   const plWords = useSelector((state) => state.wordsDataSlice.plWords);
   const wordsIds = useSelector((state) => state.wordsDataSlice.idsWords);
+  // console.log('[Pl Word]', plWords, '\n', '[Eng Word]:', engWords, '\n', '[Ids] :', wordsIds);
+  let inputValue;
+  const getInputValue = (val) => {
+    inputValue = val;
+  };
+  const [isHidden, setIsHidden] = useState(true);
+  //CSS Variable
   return (
-    <CenterContainer>
+    <CenterContainer modifiers="none">
       <Table>
-        <Row>
-          <div>
-            <strong>Lp.</strong>
-          </div>
-          <div>
-            <strong>English Word</strong>
-          </div>
-          <div>
-            <strong>Polish Word</strong>
-          </div>
-          <div>
-            <strong>Actions</strong>
-          </div>
-        </Row>
-        {engWords.map((word, index) => {
-          // i+1 cause the array start from 0 and I want to itterate from 1
-          return (
-            <Row key={wordsIds[index]}>
-              <div>{index + 1}</div>
-              <div>{word}</div>
-              <div>{plWords[index]}</div>
-              <div className="action">
-                <button className="buttonRectangle">
-                  <img alt="edit-Icon" src={editIcon}></img>
-                </button>
-                <button
-                  name="delete-item-button"
-                  className="buttonCircle"
-                  onClick={() => {
-                    deleteSingleData(user, wordsIds[index]);
-                  }}
-                >
-                  <img alt="delete-Icon" src={deleteIcon}></img>
-                </button>
-              </div>
-            </Row>
-          );
-        })}
+        <img
+          className="arrow"
+          alt="icon"
+          src={isHidden ? arrowUp : arrowDown}
+          onClick={() => {
+            setIsHidden(!isHidden);
+          }}
+        ></img>
+        <TableAdditionalOptions className={isHidden ? 'hidden-content' : 'show-content'}>
+          <Input placeholder={'Type words to filter'} sendData={getInputValue}></Input>
+          <button
+            onClick={() => {
+              console.log(inputValue);
+            }}
+          >
+            <strong>Delete All</strong>
+          </button>
+        </TableAdditionalOptions>
+        <TableItemsContainer>
+          <Row className="sticky">
+            <div>
+              <strong>Lp.</strong>
+            </div>
+            <div>
+              <strong>English Word</strong>
+            </div>
+            <div>
+              <strong>Polish Word</strong>
+            </div>
+            <div>
+              <strong>Actions</strong>
+            </div>
+          </Row>
+          {engWords.map((word, index) => {
+            // i+1 cause the array start from 0 and I want to itterate from 1
+            return (
+              <Row key={wordsIds[index]}>
+                <div>{index + 1}</div>
+                <div>{word}</div>
+                <div>{plWords[index]}</div>
+                <div className="action">
+                  <button
+                    name="edit-item-button"
+                    className="buttonRectangle"
+                    onClick={() => {
+                      alertForEditingWords();
+                    }}
+                  >
+                    <img alt="edit-Icon" src={editIcon}></img>
+                  </button>
+                  <button
+                    name="delete-item-button"
+                    className="buttonCircle"
+                    onClick={() => {
+                      deleteSingleData(user, wordsIds[index]);
+                    }}
+                  >
+                    <img alt="delete-Icon" src={deleteIcon}></img>
+                  </button>
+                </div>
+              </Row>
+            );
+          })}
+        </TableItemsContainer>
       </Table>
     </CenterContainer>
   );
--- src/views/WordsControlPanel/WordsControlPanel.style.js
+++ src/views/WordsControlPanel/WordsControlPanel.style.js
@@ -3,10 +3,70 @@ export const Table = styled.div`
   display: grid;
   width: 800px;
   text-align: center;
+  .arrow {
+    position: absolute;
+    transition: 0.5s all;
+    right: 0;
+    top: -15px;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    border: 1px solid gray;
+    cursor: pointer;
+  }
+  .arrow:hover {
+    top: -30px;
+  }
+`;
+export const TableAdditionalOptions = styled.div`
+  display: grid;
+  grid-template-columns: 80% 20%;
+  z-index: 1;
+  transition: 1s height;
+  input {
+    text-align: center;
+    padding: 10px;
+    border: 1px solid gray;
+    outline: none;
+    background: rgba(0, 0, 0, 0.397);
+    font-size: 20px;
+  }
+  button {
+    background: #0d0d0d;
+    border: 1px solid gray;
+    cursor: pointer;
+  }
+  //For CSS ANIMATION
+  &&.hidden-content {
+    height: 0px;
+  }
+  &&.show-content {
+    height: 45px;
+  }
+`;
+export const TableItemsContainer = styled.div`
+  max-height: 477px;
+  overflow-y: auto;
+  z-index: 3;
+  .sticky {
+    position: sticky;
+    top: 0;
+    background: #1a1a1a;
+  }
+  ::-webkit-scrollbar {
+    width: 15px;
+    border: 1px solid gray;
+    cursor: auto;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: rgba(0, 0, 0, 0.8);
+    border-radius: 10px;
+  }
 `;
 export const Row = styled.div`
   display: grid;
   grid-template-columns: 10% 35% 35% 20%;
+  background: rgba(0, 0, 0, 0.397);
+  box-shadow: 2px 2px 6px -1px rgb(0 0 0);
   div {
     border: 1px solid gray;
     padding: 15px;
