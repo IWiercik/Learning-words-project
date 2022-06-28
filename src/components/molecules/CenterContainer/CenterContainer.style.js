95cd85a424f767b4ae080952b1702fdb6a9fa1dc
Swiatlon <wiercikdoc@gmail.com>
2022-06-28 14:54:51 +0200

    Added modifiers option for style

--- src/components/molecules/CenterContainer/CenterContainer.style.js
+++ src/components/molecules/CenterContainer/CenterContainer.style.js
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 export const CenterContainer = styled.div`
   align-self: center;
   justify-self: center;
-  background: rgba(0, 0, 0, 0.397);
+  background: ${({ modifiers }) => (modifiers === 'none' ? 'none' : `rgba(0, 0, 0, 0.397)`)};
   min-width: 300px;
   position: relative;
   display: grid;
-  box-shadow: 2px 2px 6px -1px rgb(0 0 0);
+  box-shadow: ${({ modifiers }) => (modifiers === 'none' ? 'none' : `2px 2px 6px -1px rgb(0 0 0);`)};
+  overflow: visible ;
 `;
