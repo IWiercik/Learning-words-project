6b5481e73eb4354c53e150d08c9e5be3c86b865c
Swiatlon <wiercikdoc@gmail.com>
2022-06-30 14:28:52 +0200

    Sent input state to parent element

--- src/components/atoms/Input/Input.js
+++ src/components/atoms/Input/Input.js
@@ -1,14 +1,5 @@
-import { useState } from 'react';
-function Input({ placeholder, sendData }) {
-  const initialInputValue = '';
-  const [inputValue, setInputValue] = useState(initialInputValue);
-  const handleInput = (e) => {
-    setInputValue(e.target.value);
-  };
-  (function passDataToParent() {
-    sendData(inputValue);
-  })();
-  return <input type={'text'} placeholder={placeholder} value={inputValue} onChange={handleInput}></input>;
+function Input({ placeholder, value, onChange }) {
+  return <input type={'text'} placeholder={placeholder} value={value} onChange={onChange}></input>;
 }
 
 export default Input;
