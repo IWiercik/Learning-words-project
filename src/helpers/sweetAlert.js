75b6c9936f9a29e69c13aee7480da022b20c5d3f
Swiatlon <wiercikdoc@gmail.com>
2022-06-30 14:34:46 +0200

    Added editing data firebase connected with sweetAlerts

--- src/helpers/sweetAlert.js
+++ src/helpers/sweetAlert.js
@@ -1,6 +1,7 @@
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 import 'helpers/sweetAlert.css';
+import { editData } from 'configFirebase/firebase';
 export const alertForHints = (wordToTranslate) => {
   const maxLettersForHint = Math.round(wordToTranslate.length / 2);
   const MySwal = withReactContent(Swal);
@@ -42,9 +43,38 @@ export const alertForEmptyInput = () => {
   });
 };
 
-export const alertForEditingWords = () => {
-  Swal.fire({
-    icon: 'warning',
-    title: `Lets's goo!`,
+export const alertForEditingWords = (engWord, plWord, id, user) => {
+  const MySwal = withReactContent(Swal);
+  MySwal.fire({
+    html: `
+    <div class="flex-container">
+      <label for="engWord">English Word:</label> <input name="engWord" value="${engWord}" type="text"></input> 
+      <label for="plWord">Polish Word:</label> <input name="plWord"  value ="${plWord}"type="text "></input>
+    </div> 
+   `,
+    confirmButtonColor: '#1a1a1a',
+    confirmButtonText: 'Update data!',
+    preConfirm: function () {
+      return new Promise(function (resolve) {
+        const engWord = document.querySelector('[name="engWord"]').value;
+        const plWord = document.querySelector('[name="plWord"]').value;
+        if (engWord.length < 2 || plWord.length < 2) {
+          Swal.showValidationMessage('Fill in fields correctly!');
+          Swal.enableButtons();
+        } else {
+          Swal.resetValidationMessage();
+          resolve([engWord, plWord]);
+        }
+      });
+    },
+  }).then((result) => {
+    const engWord = result.value[0];
+    const plWord = result.value[1];
+    editData(user, id, engWord, plWord).then(() => {
+      Swal.fire({
+        icon: 'success',
+        title: 'Updated!',
+      });
+    });
   });
 };
