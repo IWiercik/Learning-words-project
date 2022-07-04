ffa0064de12f21de49547d2347b8f6f956b6a083
Swiatlon <wiercikdoc@gmail.com>
2022-07-04 17:41:19 +0200

    Added home component and changed basic container instea centered container

--- src/layout/PreAuth/PreAuth.js
+++ src/layout/PreAuth/PreAuth.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import Navigation from 'components/organisms/Navigation/Navigation';
-import { CenteredContent } from './PreAuth.style';
+import { BasicContainer } from 'components/molecules/BasicContainer/BasicContainer.style';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import RegistrationForm from 'views/RegistrationForm/Registration';
 import LoginForm from 'views/LoginForm/LoginForm';
+import Home from 'views/Home/Home';
 
 const preAuth = () => (
   <Router>
     <Navigation isAuthorized={false}></Navigation>
-    <CenteredContent>
+    <BasicContainer noPadding={true}>
       <Switch>
-        <Route exact path="/login" component={LoginForm} />
-        <Route exact path="/register" component={RegistrationForm} />
-        <Route component={LoginForm} />
+        <Route path="/home" component={Home}></Route>
+        <Route path="/login" component={LoginForm} />
+        <Route path="/register" component={RegistrationForm} />
+        <Route component={Home} />
       </Switch>
-    </CenteredContent>
+    </BasicContainer>
   </Router>
 );
 export default preAuth;
