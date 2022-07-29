1d611fb0dfaf2aa935c58f68c35471c3ef32bd52
Swiatlon <wiercikdoc@gmail.com>
2022-07-29 11:56:16 +0200

    Recreated the Home component

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -5,29 +5,49 @@ import { HomeContainer } from './Home.style';
 import { ButtonsBox } from 'views/LearningMode/LearningMode.style';
 import { ButtonLink } from './Home.style';
 import { signInUser } from 'configFirebase/firebase';
-function Home() {
+import { CenterContainer } from 'components/molecules/CenterContainer/CenterContainer.style';
+import React from 'react';
+function Home({ isAuthorized }) {
   return (
-    <HomeContainer>
-      <Title>Welcome!</Title>
-      <Text>
-        The website you are visiting helps learning words in <span>exact given translation!</span>
-      </Text>
-      <ButtonsBox>
-        <Button>
-          <ButtonLink to="/register">Get started!</ButtonLink>
-        </Button>
-        <Button>
-          <ButtonLink to="/tutorial">Tutorial</ButtonLink>
-        </Button>
-        <Button
-          onClick={() => {
-            signInUser('guest@gmail.com', 'B497M7E06iJPbrGE72xZ');
-          }}
-        >
-          Guest Account
-        </Button>
-      </ButtonsBox>
-    </HomeContainer>
+    <React.Fragment>
+      {isAuthorized ? (
+        <CenterContainer>
+          <HomeContainer>
+            <Title>Welcome!</Title>
+            <Text>
+              The website you are visiting helps learning words in <span>exact given translation!</span>
+            </Text>
+            <ButtonsBox>
+              <Button>
+                <ButtonLink to="/tutorial">Tutorial</ButtonLink>
+              </Button>
+            </ButtonsBox>
+          </HomeContainer>
+        </CenterContainer>
+      ) : (
+        <HomeContainer>
+          <Title>Welcome!</Title>
+          <Text>
+            The website you are visiting helps learning words in <span>exact given translation!</span>
+          </Text>
+          <ButtonsBox>
+            <Button>
+              <ButtonLink to="/register">Get started!</ButtonLink>
+            </Button>
+            <Button>
+              <ButtonLink to="/tutorial">Tutorial</ButtonLink>
+            </Button>
+            <Button
+              onClick={() => {
+                signInUser('quest@gmail.com', 'B497M7E06iJPbrGE72xZ');
+              }}
+            >
+              Guest Account
+            </Button>
+          </ButtonsBox>
+        </HomeContainer>
+      )}
+    </React.Fragment>
   );
 }
 
