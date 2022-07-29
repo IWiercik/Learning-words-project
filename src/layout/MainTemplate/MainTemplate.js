4fc60fe1c8f05fdc263fbcc2d089115817bc88bc
Swiatlon <wiercikdoc@gmail.com>
2022-07-29 11:55:25 +0200

    Added path to Home and Account component

--- src/layout/MainTemplate/MainTemplate.js
+++ src/layout/MainTemplate/MainTemplate.js
@@ -4,12 +4,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TypingMode from 'views/TypingMode/TypingMode';
 import LearningMode from 'views/LearningMode/LearningMode';
 import WordsControlPanel from 'views/WordsControlPanel/WordsControlPanel';
+import Home from 'views/Home/Home';
+import Account from 'views/Account/Account';
 
 const MainTemplate = () => {
   return (
     <BrowserRouter>
       <Navigation isAuthorized={true}></Navigation>
       <Routes>
+        <Route path="/home" element={<Home isAuthorized={true} />}></Route>
+        <Route path="/account" element={<Account />}></Route>
         <Route path="/typing" element={<TypingMode />}></Route>
         <Route path="/learning" element={<LearningMode />}></Route>
         <Route path="/wordsControlPanel" element={<WordsControlPanel />}></Route>
