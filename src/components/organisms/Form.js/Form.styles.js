ba8b63f2601d2093b59a65065259fc13281772cb
Swiatlon <wiercikdoc@gmail.com>
2022-07-30 16:26:15 +0200

    Updated styles and added ResetPassword as styled component

--- src/components/organisms/Form.js/Form.styles.js
+++ src/components/organisms/Form.js/Form.styles.js
@@ -1,15 +1,25 @@
 import styled from 'styled-components';
 export const Wrapper = styled.form`
   width: 300px;
-  height: 300px;
+  height: 340px;
   background: rgb(0, 0, 0, 0.3);
   border-radius: 15px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 20% 20% 20% 15% 20%;
+  align-items: end;
+  justify-items: center;
   div {
     position: relative;
     max-width: 70%;
+  }
+`;
+
+export const ResetPassword = styled.p`
+  color: gray;
+  cursor: pointer;
+  transition: 0.5s all;
+  &:hover {
+    transform: scale(1.15);
   }
 `;
