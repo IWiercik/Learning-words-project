4d24a7b2ecbb9ce9e1a9df6288a06e147014d7fe
Swiatlon <wiercikdoc@gmail.com>
2022-07-30 16:31:01 +0200

    Updated tutorial component

--- src/views/Tutorial/Tutorial.js
+++ src/views/Tutorial/Tutorial.js
@@ -7,6 +7,7 @@ import tutorial2 from 'assets/images/tutorial2.png';
 import tutorial3 from 'assets/images/tutorial3.png';
 import { Exit, TutorialImage, ArrowLeft, ArrowRight, BottomPart } from './Tutorial.style';
 import Circle from 'components/atoms/TutorialCounter/TutorialCounter';
+import { CenterContainer } from 'components/molecules/CenterContainer/CenterContainer.style';
 import { useState } from 'react';
 function Tutorial() {
   const initialState = 1;
@@ -14,35 +15,37 @@ function Tutorial() {
   const usersTextHelpers = ['Create your Account', 'Add words to database', 'Learn by practise!'];
   const usersTextImages = [tutorial1, tutorial2, tutorial3];
   return (
-    <TutorialContainer>
-      <h2>Tutorial</h2>
-      <Exit to="/home">
-        <img src={exit} alt="exit" />
-      </Exit>
-      <TutorialImage src={usersTextImages[tutorialCounter - 1]} alt="tutorial" />
-      <h4>{usersTextHelpers[tutorialCounter - 1]} </h4>
-      <BottomPart>
-        <ArrowLeft
-          src={arrowLeft}
-          alt="arrow-left"
-          onClick={() => {
-            if (tutorialCounter > 1) {
-              setTutorialCounter(tutorialCounter - 1);
-            }
-          }}
-        />
-        <Circle text={tutorialCounter} />
-        <ArrowRight
-          src={arrowRight}
-          alt="arrow-right"
-          onClick={() => {
-            if (tutorialCounter < 3) {
-              setTutorialCounter(tutorialCounter + 1);
-            }
-          }}
-        />
-      </BottomPart>
-    </TutorialContainer>
+    <CenterContainer>
+      <TutorialContainer>
+        <h2>Tutorial</h2>
+        <Exit to="/home">
+          <img src={exit} alt="exit" />
+        </Exit>
+        <TutorialImage src={usersTextImages[tutorialCounter - 1]} alt="tutorial" />
+        <h4>{usersTextHelpers[tutorialCounter - 1]} </h4>
+        <BottomPart>
+          <ArrowLeft
+            src={arrowLeft}
+            alt="arrow-left"
+            onClick={() => {
+              if (tutorialCounter > 1) {
+                setTutorialCounter(tutorialCounter - 1);
+              }
+            }}
+          />
+          <Circle text={tutorialCounter} />
+          <ArrowRight
+            src={arrowRight}
+            alt="arrow-right"
+            onClick={() => {
+              if (tutorialCounter < 3) {
+                setTutorialCounter(tutorialCounter + 1);
+              }
+            }}
+          />
+        </BottomPart>
+      </TutorialContainer>
+    </CenterContainer>
   );
 }
 
